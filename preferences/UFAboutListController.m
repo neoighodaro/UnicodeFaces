34b0b63cc33cbe5e58619b89b8e6692184470bc9
Neo Ighodaro <neo@creativitykills.co>
2015-12-12 08:51:32 +0100

    Readability improvements

--- preferences/UFAboutListController.m
+++ preferences/UFAboutListController.m
@@ -1,11 +1,15 @@
 #import "UFAboutListController.h"
 
 @implementation UFAboutListController
+
 #pragma mark - Constants
+
 + (NSString *)hb_supportEmailAddress {
 	return @"tweaks@tapsharp.com";
 }
+
 + (NSString *)hb_specifierPlist {
 	return @"About";
 }
+
 @end
