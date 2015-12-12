d2da6f4a6cb3d888cc6c1ee54e8280850d807921
Neo Ighodaro <neo@creativitykills.co>
2015-12-12 08:51:55 +0100

    Updated headers

--- preferences/UFReorderListController.h
+++ preferences/UFReorderListController.h
@@ -11,9 +11,23 @@ extern NSString* PSDeletionActionKey;
 - (BOOL)performDeletionActionForSpecifier:(PSSpecifier*)specifier;
 @end
 
+
 @interface UFReorderListController : PSEditableListController {
 	NSMutableArray* _unicodeFaces;
 	BOOL _isEditingMode;
 }
+
 @property (retain, nonatomic) NSMutableArray* unicodeFaces;
+
+- (id)init;
+- (id)specifiers;
+- (NSMutableArray *)unicodeFaces;
+- (NSMutableArray *)unifacecodeSpecifiers;
+- (PSSpecifier *)unifacecodeSpecifier:(NSString *)uniface;
+- (id)readPreferenceValue:(PSSpecifier *)specifier;
+- (void)removeUnicodeFace:(PSSpecifier *)specifier;
+- (void)addUnicodeFace:(NSString *)unicodeFace;
+- (void)updateUnicodeFace:(NSString *)unicodeFace atIndex:(NSIndexPath *)indexPath;
+- (IBAction)addUnicodeFaceAlert:(id)sender;
+- (void)editUnicodeFaceAlertForIndex:(NSIndexPath *)indexPath;
 @end
